b9d1d6198ee029886fc54a779a220822b2538c7e
Ilja <ilja@spinn.ee>
2021-06-20 15:03:34 +0300

    Remove the now ignored directory "build"

--- build/googletest-prefix/src/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/iljakrusman/Git/arduino-gtest/build/googletest-prefix/src/googletest/googletest
-# Build directory: /Users/iljakrusman/Git/arduino-gtest/build/googletest-prefix/src/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
